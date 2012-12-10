98a639becdd1b746d90b61963d998aafb17ebad4
Frank Bergmann <frank.bergmann@project-open.com>
2012-12-10 13:13:47 +0000

    - Workflow:   Fixed URL for workflow GIF

--- www/admin/workflow-summary.tcl
+++ www/admin/workflow-summary.tcl
@@ -180,19 +180,15 @@ if { $simple_p } {
 if { [wf_graphviz_installed_p] } {
 
     set dot_text [wf_generate_dot_representation workflow]
-    
     set tmpfile [wf_graphviz_dot_exec -to_file -output gif $dot_text]
-    
     set width_and_height ""
     if { ![catch { set image_size [ns_gifsize $tmpfile] } error] } {
 	if { ![empty_string_p $image_size] } {
 	    set width_and_height "width=[lindex $image_size 0] height=[lindex $image_size 1]"
 	}
     }
-    
     ad_set_client_property wf wf_net_tmpfile $tmpfile
-    
-    set workflow_img_tag "<img src=\"[im_workflow_url]/workflow-gif?[export_url_vars tmpfile]\" border=0 $width_and_height alt=\"Graphical representation of the process network\">"
+    set workflow_img_tag "<img src=\"/[im_workflow_url]/workflow-gif?[export_url_vars tmpfile]\" border=0 $width_and_height alt=\"Graphical representation of the process network\">"
     
 } else {
     set workflow_img_tag ""
