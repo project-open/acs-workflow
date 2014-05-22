83aceebff60f3a72e4744be08e353adb342d6f76
Frank Bergmann <frank.bergmann@project-open.com>
2014-05-22 09:31:56 +0000

    - beautified

--- www/admin/arc-edit.tcl
+++ www/admin/arc-edit.tcl
@@ -32,33 +32,10 @@ set guard_custom_arg [ad_quotehtml $guard_custom_arg]
 # must match
 # Ugh! This is ugly. We really need that callback repository!
 
-set possible_guards [util_memoize {
-db_list possible_guards {
-    select a0.package_name || '.' || a0.object_name
-    from   user_arguments a0
-    where  position = 0
-    and    argument_name is null
-    and    data_type = 'CHAR'
-    and    in_out = 'OUT'
-    and    exists (select 1 from user_arguments a1 where a1.package_name=a0.package_name and a1.object_name=a0.object_name 
-                   and a1.position=1 and a1.data_type='NUMBER' and a1.in_out='IN')
-    and    exists (select 1 from user_arguments a2 where a2.package_name=a0.package_name and a2.object_name=a0.object_name 
-                   and a2.position=2 and a2.data_type='VARCHAR2' and a2.in_out='IN')
-    and    exists (select 1 from user_arguments a3 where a3.package_name=a0.package_name and a3.object_name=a0.object_name 
-                   and a3.position=3 and a3.data_type='VARCHAR2' and a3.in_out='IN')
-    and    exists (select 1 from user_arguments a4 where a4.package_name=a0.package_name and a4.object_name=a0.object_name 
-                   and a4.position=4 and a4.data_type='VARCHAR2' and a4.in_out='IN')
-    and    exists (select 1 from user_arguments a5 where a5.package_name=a0.package_name and a5.object_name=a0.object_name 
-                   and a5.position=5 and a5.data_type='VARCHAR2' and a5.in_out='IN')
-    and    exists (select 1 from user_arguments a6 where a6.package_name=a0.package_name and a6.object_name=a0.object_name 
-                   and a6.position=6 and a6.data_type='VARCHAR2' and a6.in_out='IN')
-}} 3600]
-
+set possible_guards [util_memoize [list db_list possible_guards ""] 3600]
 
 template::multirow create guard_options value selected name
-
 template::multirow append guard_options "" [ad_decode $guard_callback "" "SELECTED" ""] "--no guard-- [ad_decode $guard_callback "" "(current)" ""]"
-
 template::multirow append guard_options "#" [ad_decode $guard_callback "#" "SELECTED" ""] "No other guards were satsified [ad_decode $guard_callback "#" "(current)" ""]"
 
 if { ![empty_string_p $guard_callback] && ![string equal $guard_callback "#"] && \
